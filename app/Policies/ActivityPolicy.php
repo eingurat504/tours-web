fe12c3b5cebef31498ba470e3f52e8ee1a212e2c
Emmy Ingurat <eingurat504@gmail.com>
2023-11-19 23:33:37 +0300

    activity policy updated

--- app/Policies/ActivityPolicy.php
+++ app/Policies/ActivityPolicy.php
@@ -9,13 +9,88 @@ class ActivityPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Create a new policy instance.
+
+  /**
+     * Determine whether the user can view any role.
      *
-     * @return void
+     * @param \App\Models\User $user
+     *
+     * @return bool
      */
-    public function __construct()
+    public function viewAny(User $user)
     {
-        //
+        return $user->can('view-any activities');
+    }
+
+    /**
+     * Determine whether the user can view a role.
+     *
+     * @param \App\Models\User $user
+     * @param int $activityId
+     *
+     * @return bool
+     */
+    public function view(User $user, int $activityId)
+    {
+        if ($user->role_id == $activityId) {
+            return true;
+        }
+
+        return $user->can('view activities');
+    }
+
+    /**
+     * Determine whether the user can create activities.
+     *
+     * @param \App\Models\User $user
+     *
+     * @return bool
+     */
+    public function create(User $user)
+    {
+        return $user->can('create activities');
+    }
+
+    /**
+     * Determine whether the user can update a role.
+     *
+     * @param \App\Models\User $user
+     * @param int $activityId
+     *
+     * @return bool
+     */
+    public function update(User $user, int $activityId)
+    {
+        if ($user->id == $activityId) {
+            return true;
+        }
+
+        return $user->can('update activities');
+    }
+
+    /**
+     * Determine whether the user can delete a role.
+     *
+     * @param \App\Models\User $user
+     * @param int              $activityId
+     *
+     * @return bool
+     */
+    public function delete(User $user, int $activityId)
+    {
+        return $user->can('delete activities');
+    }
+
+    /**
+     * Determine whether the user can sync role permissions.
+     *
+     * @param \App\Models\User $user
+     * @param int              $activityId
+     *
+     * @return bool
+     */
+    public function syncPermissions(User $user, int $activityId)
+    {
+        return $user->can('sync-permissions activities');
     }
 }
