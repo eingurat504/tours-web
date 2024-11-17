a4c1296630ba1862845a3f3f9842d9c144ac79a4
Emmy Ingurat <eingurat504@gmail.com>
2024-11-17 14:50:13 +0300

    create booking ui

--- app/DataTables/BookingsDataTable.php
+++ app/DataTables/BookingsDataTable.php
@@ -2,7 +2,7 @@
 
 namespace App\DataTables;
 
-use App\Models\Activity;
+use App\Models\Booking;
 use Illuminate\Database\Eloquent\Builder as QueryBuilder;
 use Yajra\DataTables\EloquentDataTable;
 use Yajra\DataTables\Html\Builder as HtmlBuilder;
@@ -10,54 +10,85 @@ use Yajra\DataTables\Html\Button;
 use Yajra\DataTables\Html\Column;
 use Yajra\DataTables\Html\Editor\Editor;
 use Yajra\DataTables\Html\Editor\Fields;
+use Illuminate\Support\Facades\Auth;
 use Yajra\DataTables\Services\DataTable;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class BookingsDataTable extends DataTable
 {
+
+
+        /**
+     * Currently authenticated user.
+     *
+     * @var \Illuminate\Contracts\Auth\Access\Authorizable
+     */
+    protected $user;
+
+    // protected $request;
+
+        /**
+     * Constructor.
+     *
+     * @param \Illuminate\Http\Request $request
+     */
+    public function __construct(Request $request)
+    {
+        // dd(auth()->user()->userable->farmer_union_id);
+    //     $this->user = $request->user();
+   
+    }
+    
     /**
      * Build DataTable class.
      *
      * @param QueryBuilder $query Results from query() method.
      * @return \Yajra\DataTables\EloquentDataTable
      */
-    public function dataTable(QueryBuilder $query): EloquentDataTable
+    public function dataTable($query)
     {
-        return (new EloquentDataTable($query))
-        ->editColumn('created_at', function ($request) {
-            return $request->created_at->format('Y-m-d H:i:s'); // human readable format
-        })
-        ->editColumn('updated_at', function ($request) {
-            return $request->updated_at->format('Y-m-d H:i:s'); // human readable format
-        })
-        ->addColumn('actions', function ($booking) {
-            $actions = $this->buildActions($booking);
+        return datatables()
+                ->eloquent($query)
+                ->addColumn('category_id', function ($booking) {
+                    return $booking->category->name ?? '';
+                })
+                ->addColumn('status', function ($delivery) {
+                    if ($delivery->status == 1) {
+                        return '<span class="inline-block bg-red-500 text-white text-xs font-semibold px-2 py-1 rounded">UnPublished</span>';
+                    } else {
+                        return '<span class="inline-block bg-green-500 text-white text-xs font-semibold px-2 py-1 rounded">Published</span>';
+                    }
+                })
+                ->editColumn('created_at', function ($request) {
+                    return $request->created_at->format('Y-m-d H:i:s'); // human readable format
+                })
+                ->editColumn('updated_at', function ($request) {
+                    return $request->updated_at->format('Y-m-d H:i:s'); // human readable format
+                })
+                ->addColumn('actions', function ($booking) {
+                    $actions = $this->buildActions($booking);
 
-            return '
-            <div class="dropdown show">
-                <div class="text-muted" data-toggle="dropdown">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                        class="feather feather-more-horizontal">
-                        <circle cx="12" cy="12" r="1"></circle>
-                        <circle cx="19" cy="12" r="1"></circle>
-                        <circle cx="5" cy="12" r="1"></circle>
-                    </svg>
-                </div>
-                <div class="dropdown-menu">'.$actions.'</div>
-            </div>';
-        })
-        ->rawColumns(['actions']);
+                    return '
+                        <ul class="flex gap-2 list-none mb-0">
+                           '.$actions.'
+                        </ul>';
+                })
+                ->rawColumns(['actions','status','category_id']);
     }
 
     /**
      * Get query source of dataTable.
      *
-     * @param \App\Models\Activity $model
+     * @param \App\Models\User $model
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function query(Booking $model): QueryBuilder
+    public function query(Booking $booking)
     {
-        return $model->newQuery();
+        $query = $booking->newQuery();
+
+        return $query;
     }
 
     /**
@@ -67,13 +98,30 @@ class BookingsDataTable extends DataTable
      */
     public function html(): HtmlBuilder
     {
+
         return $this->builder()
                     ->setTableId('bookings-table')
                     ->columns($this->getColumns())
+                    ->language([
+                        'emptyTable' => "No bookings available",
+                        'info' => "Showing _START_ to _END_ of _TOTAL_ bookings",
+                        'infoEmpty' => "Showing 0 to 0 of 0 bookings",
+                        'infoFiltered' => "(filtered from _MAX_ total bookings)",
+                        'infoPostFix' => "",
+                        'thousands' => ",",
+                        'lengthMenu' => "Show _MENU_ bookings",
+                        'search' => 'Search bookings:',
+                        'zeroRecords' => 'No bookings match search criteria'
+                    ])
                     ->minifiedAjax()
-                    //->dom('Bfrtip')
-                    ->orderBy(1)
-                    ->selectStyleSingle()
+                    // ->addCheckbox()
+                    ->orderBy(4, 'desc')
+                    // ->pageLength(10)
+                    ->responsive()
+                    ->parameters([
+                        "responsive" => true,
+                        'buttons' => ['excel', 'print', 'pdf'],
+                    ])
                     ->buttons([
                         Button::make('excel'),
                         Button::make('csv'),
@@ -91,18 +139,18 @@ class BookingsDataTable extends DataTable
      */
     public function getColumns(): array
     {
-        return [
-            Column::computed('actions')
-                  ->exportable(false)
-                  ->printable(false)
-                  ->width(60)
-                  ->addClass('text-center'),
-            Column::make('id'),
-            Column::make('activity_name'),
-            Column::make('duration'),
-            Column::make('amount'),
+        return [    
+            Column::make('booking_no'),
+            Column::make('traveller_name'),
+            Column::make('traveller_flight_no'),
+            Column::make('total_amount'),
             Column::make('created_at'),
             Column::make('updated_at'),
+            Column::computed('actions')
+            ->exportable(false)
+            ->printable(false)
+            ->width(60)
+            ->addClass('text-center'),
         ];
     }
 
@@ -113,69 +161,48 @@ class BookingsDataTable extends DataTable
      */
     protected function filename(): string
     {
-        return 'Activities_' . date('YmdHis');
+        return 'bookings_' . date('YmdHis');
     }
 
-            /**
+        /**
      * Build actions.
      *
      * @param $resource
      * @return string
      */
-    protected function buildActions($activity)
+    protected function buildActions($booking)
     {
         /*if ($this->user == null) {
             return '';
         }*/
 
         $routes = [
-            'view' => route('activities.show', $activity->id),
-            'edit' => route('activities.edit', $activity->id),
-            // 'destroy' => route('projects.projects.destroy',$project->id),
+            'view' => route('bookings.show', $booking->id),
+            'uploads' => route('bookings.uploads',$booking->id)
         ];
 
-        $actions = '';
+        $actions = ' ';
 
-        // if ($this->user->can('view activities')) {
+        // if ($this->user->can('view bookings')) {
             $actions .= '
-            <a class="dropdown-item d-flex" href="' . $routes['view'] . '">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
-                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                    class="feather feather-eye">
-                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                    <circle cx="12" cy="12" r="3"></circle>
-                </svg>
-                <span class="ml-2">View<span>
-            </a>';
+            <li>
+                <a href="' . $routes['view'] . '" class="bg-indigo-600 text-white px-2 py-1 rounded-md flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-white-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+                    </svg>
+                </a>
+            </li>';
         // }
 
-        // if ($this->user->can('update activities')) {
+            // if ($this->user->can('view bookings')) {
             $actions .= '
-            <a class="dropdown-item d-flex" href="' . $routes['edit'] . '">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
-                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                    class="feather feather-edit">
-                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                </svg>
-                <span class="ml-2">Edit</span>
-            </a>';
-        // }
-
-        // if ($this->user->can('delete activities')) {
-        //     $actions .= '
-        //      <a class="dropdown-item d-flex" href="#" data-id="'.$booking->id.'"
-        //          data-title="'.$booking->title.'" data-route="'.$routes['destroy'].'"
-        //          data-toggle="modal" data-target="#destroy-entity-modal">
-        //          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
-        //              fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-        //              class="feather feather-trash">
-        //              <polyline points="3 6 5 6 21 6"></polyline>
-        //              <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-        //          </svg>
-        //          <span class="ml-2">Delete</span>
-        //      </a>';
-        // }
+            <li>
+                <a href="' . $routes['uploads'] . '" class="bg-indigo-600 text-white px-2 py-1 rounded-md flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-white-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+                    </svg>
+                </a>
+            </li>';      
 
         return $actions;
     }
