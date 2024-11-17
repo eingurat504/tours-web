518cc78e415495b237a96d7184e8c3ed08b1e6e7
Emmy Ingurat <eingurat504@gmail.com>
2024-11-17 12:04:54 +0300

    code update

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -18,43 +18,43 @@ class PermissionsSeeder extends Seeder
     {
       
         
-        \DB::table('permissions')->delete();
+        // \DB::table('permissions')->delete();
 
-        \DB::table('permissions')->insert(array (
-            0 =>
-            array (
-                'id' => 1,
-                'name' => 'ViewSystemLogs',
-                'guard_name' => 'web',
-                'created_at' => '2018-11-14 13:35:58',
-                'updated_at' => '2018-11-14 13:35:58',
-            ),
-            1 =>
-            array (
-                'id' => 2,
-                'name' => 'ManageSystemBackups',
-                'guard_name' => 'web',
-                'created_at' => '2018-11-14 13:44:23',
-                'updated_at' => '2018-11-14 13:44:23',
-            ),
-            2 =>
-            array (
-                'id' => 3,
-                'name' => 'ManagePermissions',
-                'guard_name' => 'web',
-                'created_at' => '2018-11-14 13:45:07',
-                'updated_at' => '2018-11-14 13:45:07',
-            ),
-            3 =>
-            array (
-                'id' => 4,
-                'name' => 'ViewUsers',
-                'guard_name' => 'web',
-                'created_at' => '2018-11-14 14:09:52',
-                'updated_at' => '2018-11-14 14:09:52',
-            )
+        // \DB::table('permissions')->insert(array (
+        //     0 =>
+        //     array (
+        //         'id' => 1,
+        //         'name' => 'ViewSystemLogs',
+        //         'guard_name' => 'web',
+        //         'created_at' => '2018-11-14 13:35:58',
+        //         'updated_at' => '2018-11-14 13:35:58',
+        //     ),
+        //     1 =>
+        //     array (
+        //         'id' => 2,
+        //         'name' => 'ManageSystemBackups',
+        //         'guard_name' => 'web',
+        //         'created_at' => '2018-11-14 13:44:23',
+        //         'updated_at' => '2018-11-14 13:44:23',
+        //     ),
+        //     2 =>
+        //     array (
+        //         'id' => 3,
+        //         'name' => 'ManagePermissions',
+        //         'guard_name' => 'web',
+        //         'created_at' => '2018-11-14 13:45:07',
+        //         'updated_at' => '2018-11-14 13:45:07',
+        //     ),
+        //     3 =>
+        //     array (
+        //         'id' => 4,
+        //         'name' => 'ViewUsers',
+        //         'guard_name' => 'web',
+        //         'created_at' => '2018-11-14 14:09:52',
+        //         'updated_at' => '2018-11-14 14:09:52',
+        //     )
           
-        ));
+        // ));
 
   
     }
--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -17,19 +17,19 @@ class RolesSeeder extends Seeder
     public function run()
     {
       
-        \DB::table('roles')->delete();
+        // \DB::table('roles')->delete();
 
-        \DB::table('roles')->insert(array (
-            0 =>
-            array (
-                'id' => 1,
-                'name' => 'System Administrator',
-                'guard_name' => 'web',
-                'created_at' => '2018-11-14 13:36:17',
-                'updated_at' => '2018-11-14 13:36:17',
-            ),
+        // \DB::table('roles')->insert(array (
+        //     0 =>
+        //     array (
+        //         'id' => 1,
+        //         'name' => 'System Administrator',
+        //         'guard_name' => 'web',
+        //         'created_at' => '2018-11-14 13:36:17',
+        //         'updated_at' => '2018-11-14 13:36:17',
+        //     ),
          
-        ));
+        // ));
   
     }
 }
